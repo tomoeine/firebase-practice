0b06e97beff10955114e6018ee3f865848fc01e1
Tomoe Sawai <tomoe.teshima@gmail.com>
2021-03-23 11:49:01 +0900

    [add] functions のサンプルコード

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
 const functions = require("firebase-functions");
+const firebase = require("firebase-admin");
+const express = require("express");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const firebaseConfig = functions.config() ? functions.config().firebase : {};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firestore = firebaseApp.firestore();
+
+const app = express();
+const api = express.Router();
+
+app.use(express.json());
+
+api.post("/authors", (req, res) => {
+  const authorParams = req.body;
+  firestore
+    .collection("authors")
+    .add(authorParams)
+    .then(() => res.json({ message: "ok" }))
+    .catch((e) => {
+      console.error(e);
+    });
+});
+
+app.use("/api", api);
+
+exports.app = functions.https.onRequest(app);
